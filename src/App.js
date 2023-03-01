00bf67ebc732fc108e72bc75775e233bc7aa7007
kaikpizzi <kaik1011@hotmail.com>
2023-03-01 15:10:34 -0300

    segunda commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TodoList from './components/todolist';
-import Tarefas from './reactModal/createTask';
 
 function App() {
   return (
